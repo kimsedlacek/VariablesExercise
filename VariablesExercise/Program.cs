1859e47377948b4e8348c8ff1040d3ead475cae9
Kim Sedlacek <sedlaceks@optonline.net>
2023-11-03 12:02:59 -0400

    Cheryl's puppies

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string memberName = "Cheryl B.";
+            int numberOfPuppiesRaised = 3;
+            char sexPreference = 'F';
+            bool readyForNextPuppy = true;
+            double vetVisits = 5;
+            decimal crossBread = 62.6m;
+
+            Console.WriteLine($"{memberName} has raised {numberOfPuppiesRaised} dogs for The Seeing Eye.  Her family" +
+                $"prefers to raise {sexPreference} dogs.  They are ready for their next puppy, {readyForNextPuppy}!" +
+                $"During the first four months they will visit the vet {vetVisits} times. This is a decimal{crossBread}");
+
+            Console.WriteLine("Is this all true Cheryl?");
+            Console.ReadLine();
+
         }
     }
 }
